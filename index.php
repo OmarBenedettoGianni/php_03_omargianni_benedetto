014c592da63e45bcb41f9d92aeda88199a757ceb
OmarBenedettoGianni <omarbenedetto200012@gmail.com>
2026-09-12 15:54:22 +0200

    inizializzati array e aggiunti nuovi valori

---
+++ index.php
@@ -0,0 +1,4 @@
+<?php
+$words1 = ['vostro',67,'essere','colle','mi','sempre',['oscura','era',89,['mezzo','E'],'ritrovai','per'],'diritta'];
+$words2 = ['elemento1' => 25.89,'elemento2' => 'possa','elemento3' => ['Virgilio','Favore','favore',['fortuna']],'fine' => '!'];
+array_push($words1, `Nel`, `mezzo`, `di`, `cammin`, `nostra`, `vita`, `una`, `selva`, `che'`, `la`, `via`, `smarrita`);
